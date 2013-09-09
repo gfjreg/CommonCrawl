7c5a0b5b6973d8bbfb3828d744c7ee9ab6725eba
akshay bhat <akshayubhat@gmail.com>
2013-09-09 19:35:43 -0400

    updated read me

--- AWS/settings.py
+++ AWS/settings.py
@@ -8,21 +8,8 @@ DEBUG = True
 try:
     from keys import AWS_KEY,AWS_SECRET,PASSCODE
 except:
-        print "Keys not set, please enter AWS key and secret to continue"
-        import os
-        fh = open('keys.py','w')
-        key = raw_input('enter AWS key')
-        secret = raw_input('enter AWS Secret')
-        passcode = raw_input('enter passcode for the app engine')
-        fh.write("AWS_KEY = '%s'; AWS_SECRET = '%s'; PASSCODE = '%s'"%(key,secret,passcode))
-        fh.close()
-        print "information stored in keys.py"
-        print "testing if keys can be imported"
-        try:
-            from keys import AWS_KEY,AWS_SECRET
-        except:
-            print "failed to import keys.py"
-            raise ImportError
+    print "failed to import keys.py"
+    raise ImportError
 
 if platform.system() == 'Darwin': # local environment detected by
     STORE_PATH = '/Users/aub3/Data/trawl/' # used for local testing
--- readme.md
+++ readme.md
@@ -20,6 +20,29 @@ AWS-EC2 Indexers:
 AWS-SQS:
     For managing common crawl files across multiple machines
 
+Setting up
+-----------
+
+AWS:
+1. Setting up indexer.py
+2. add keys.py to AWS folder
+
+    AWS/keys.py
+    AWS_KEY = '<your AWS key>'
+    AWS_SECRET = '<your AWS secret>'
+    PASSCODE = 'Any short random passcode. make sure it is same in both files'
+
+
+App Engine:
+
+1. update app.yaml with your app identifier
+2. add keys.py to AppEngine folder
+    AppEngine/keys.py
+    AWS_KEY = '<your AWS key>'
+    AWS_SECRET = '<your AWS secret>'
+    PASSCODE = 'Any short random passcode. make sure it is same in both files'
+
+
 References
 -------
 [Library used for reading sequencefiles in python](https://github.com/matteobertozzi/Hadoop/tree/master/python-hadoop)

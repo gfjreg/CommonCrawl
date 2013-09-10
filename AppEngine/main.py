1e5a89b1347997e06bfcb0a69c13d60952ba7fde
akshay bhat <akshayubhat@gmail.com>
2013-09-09 21:01:27 -0400

    started developing admin, pre gym commit

--- AppEngine/main.py
+++ AppEngine/main.py
@@ -19,13 +19,24 @@ class Search(BaseRequestHandler):
         self.generate_json("success")
 
 
+def get_status():
+    status = {}
+    status['indexer_list'] = []
+    all_files_indexed = []
+    for i in Indexer.query():
+        status['indexer_list'].append((i.pid,i.last_contact,len(i.files_processed),i.entries))
+        all_files_indexed.append(i.last_contact)
+    status['totat_metadata'] = len(METADATA_FILES)
+    status['processed_metadata'] = len(all_files_indexed)
+    status['inprocess_metadata'] = len(METADATA_FILES)-len(all_files_indexed)
+    return status
 
 class Admin(BaseRequestHandler):
     def get(self):
         user = users.get_current_user()
         if user:
             if users.is_current_user_admin():
-                self.generate('admin.html')
+                self.generate('admin.html',get_status())
                 return
             else:
                 self.redirect("/")
@@ -34,6 +45,18 @@ class Admin(BaseRequestHandler):
             self.redirect(users.CreateLoginURL("/Admin"))
             return
 
+    def post(self):
+        user = users.get_current_user()
+        if user:
+            if users.is_current_user_admin():
+                self.generate('admin.html',get_status())
+                return
+            else:
+                self.redirect("/")
+                return
+        else:
+            self.redirect(users.CreateLoginURL("/Admin"))
+            return
 
 
 
